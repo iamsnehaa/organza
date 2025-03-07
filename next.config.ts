3341e04316b7821fec065d443d9cab8231a59bf1
sneha sharma <snehasharma@snehas-MacBook-Air.local>
2025-03-08 03:53:47 +0530

    seting up prisma and few updates

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ["res.cloudinary.com"], // Allow Cloudinary images
+  },
+  
 };
+
 
 export default nextConfig;
